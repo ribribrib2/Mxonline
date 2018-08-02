a231dae7d2447f1e2a187bfc9c003f2c40c6b346
HDU-RIB\HDU-rib <1045602705@qq.com>
2018-08-03 00:42:28 +0800

    Configuring xadmin background content

--- apps/organization/adminx.py
+++ apps/organization/adminx.py
@@ -1,7 +1,34 @@
+# organization/adminx.py
+
 import xadmin
 
-from organization.models import CityDict,CourseOrg,Teacher
+from organization.models import CityDict, CourseOrg, Teacher
 
-xadmin.site.register(CityDict)
-xadmin.site.register(CourseOrg)
-xadmin.site.register(Teacher)
+
+class CityDictAdmin(object):
+    '''城市'''
+
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    '''机构'''
+
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums', 'add_time']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums', 'city__name', 'address', 'add_time']
+
+
+class TeacherAdmin(object):
+    '''老师'''
+
+    list_display = ['name', 'org', 'work_years', 'work_company', 'add_time']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org__name', 'name', 'work_years', 'work_company', 'click_nums', 'fav_nums', 'add_time']
+
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
